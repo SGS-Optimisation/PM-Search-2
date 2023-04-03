84266a2af34f9ac22831d3f3b80226686e5a383f
khushboo.chhabra <khushbooChhabra.ManmohanSingh@sgsco.com>
2023-04-03 17:34:24 +0530

    Adding typescript libraries and edited config files.

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -5,16 +5,18 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import type { DefineComponent } from "vue";
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
+// @ts-ignore
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
+         createApp({ render: () => h(App, props) })
             .use(plugin)
-            .use(ZiggyVue, Ziggy)
+            .use(ZiggyVue, (window as any).Ziggy)
             .mount(el);
     },
     progress: {
