55f8e0d21b489aac9d2fd5e0921aeff30737682e
Yann Labour <yann.labour@sgsco.com>
2023-04-11 15:00:47 +0400

    fix sort direction and use prime icon

--- resources/js/Pages/Search/Show.vue
+++ resources/js/Pages/Search/Show.vue
@@ -170,22 +170,13 @@ const layout = ref('grid');
 
                                 <div class="flex flex-col ml-2">
                                     <a class="cursor-pointer" @click="userPreferences.sortOrder = 1"
-                                       :class="{'text-blue-500': userPreferences.sortOrder === -1}">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-6" fill="none"
-                                             viewBox="0 0 24 24"
-                                             stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                  d="M5 15l7-7 7 7"/>
-                                        </svg>
+                                       :class="{'text-blue-500': userPreferences.sortOrder === 1}">
+                                        <i class="pi pi-chevron-up"></i>
+
                                     </a>
                                     <a class="cursor-pointer" @click="userPreferences.sortOrder = -1"
-                                       :class="{'text-blue-500': userPreferences.sortOrder === 1}">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-6" fill="none"
-                                             viewBox="0 0 24 24"
-                                             stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                  d="M19 9l-7 7-7-7"/>
-                                        </svg>
+                                       :class="{'text-blue-500': userPreferences.sortOrder === -1}">
+                                        <i class="pi pi-chevron-down"></i>
                                     </a>
                                 </div>
                             </div>
