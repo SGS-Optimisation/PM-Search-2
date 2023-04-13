626951ce9bd3a75e302e5b0628e4f9c5978125e4
Yann Labour <yann.labour@sgsco.com>
2023-04-13 09:58:28 +0400

    fix text search

--- resources/js/Pages/Search/TextSearch.vue
+++ resources/js/Pages/Search/TextSearch.vue
@@ -2,57 +2,42 @@
 import {Head, useForm, usePage} from "@inertiajs/vue3";
 import {defineComponent, watch, ref, reactive} from "vue";
 import JetFormSection from '@/Components/Jetstream/FormSection.vue';
-import JetInputError from '@/Components/Jetstream/InputError.vue';
-//import VueTagsInput from '@johmun/vue-tags-input'; // npm i -D <name of package>
 import route from "ziggy-js";
-import InputSwitch from 'primevue/inputswitch';
+import Button from 'primevue/button';
+import SelectButton from 'primevue/selectbutton';
+import AppLayout from "@/Layouts/AppLayout.vue";
+import Vue3TagsInput from 'vue3-tags-input';
+
+defineOptions({layout: AppLayout})
+
 
 const form = useForm({
     search_string: Array,
-    operator: Boolean,
-    isPhrase: Boolean,
-    isOCR: Boolean,
-    type: String,
+    operator: <String>'and',
 })
 const tags = ref([])
 const resetOnSuccess = ref<boolean>(false)
-const advanced = ref<boolean>(false)
+const operatorOptions = ref([
+    { name: 'And', value: 'and' },
+    { name: 'Or', value: 'or' },
+]);
+
+function handleChangeTag(event) {
+    console.log('tags', event);
+    tags.value = event;
+}
 
 function doSearch(this: any) {
-    this.form.search_string = this.tags.map((item) => item.text);
 
-    this.form.post(route('search.store'), {
+    form.transform(data => ({
+        ...data,
+        search_string: tags.value,
+    })).post(route('search.store'), {
         preserveScroll: true
     })
 }
 
-function onChangeFilterCondition(this: any, condition) {
-    console.log('switching operator');
-    this.operator = condition;
 
-    this.form.operator = this.operator ? 'and' : 'or';
-}
-
-function onChangePhraseMode(this: any, state) {
-    console.log('switching phrase mode');
-    this.phraseMode = state;
-
-    this.form.isPhrase = this.phraseMode;
-}
-
-function onChangeOCR(this: any, state) {
-    console.log('switching ocr');
-    this.ocr = state;
-
-    this.form.isOCR = this.ocr;
-}
-
-function onChangeType(this: any, state) {
-    console.log('switching type');
-    this.type = state;
-
-    this.form.type = this.type ? 'thumbnail' : 'mysgs';
-}
 </script>
 
 <template>
@@ -68,14 +53,11 @@ function onChangeType(this: any, state) {
                 <div class="col-span-3 ">
                     <div class="items-center">
                         <!-- <jet-label for="search_string" value="Query" />-->
-                        <vue-tags-input
-                            v-model="tag"
+                        <vue3-tags-input
                             :tags="tags"
                             :max-tags=8
-                            :save-on-key="saveOnKey"
-                            :add-on-key="saveOnKey"
                             placeholder="Search"
-                            @tags-changed="newTags => tags = newTags"
+                            @on-tags-changed="handleChangeTag"
                         />
                         <!-- <jet-input-error :message="form.error('search_string')" class="mt-2"/> -->
                     </div>
@@ -83,76 +65,13 @@ function onChangeType(this: any, state) {
 
                 <div class="">
                     <div class="flex flex-col">
-                        <InputSwitch v-model="checked"
-                                     onchange="onChangeFilterCondition"
-                                     :initial-state="form.operator==='and' ? '1' : '0'"
-                                     left-text="OR"
-                                     right-text="AND"/>
-                    </div>
-                </div>
-                <div>
-                    <div class="flex flex-row justify-center cursor-pointer text-xs bg-gray-100 z-50 mt-2 border border-gray-700 rounded-lg"
-                         @click="advanced=!advanced">
-                        Advanced
-                        <template v-if="advanced">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-3" fill="none" viewBox="0 0 24 24"
-                                 stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                      d="M19 13l-7 7-7-7m14-8l-7 7-7-7"/>
-                            </svg>
-                        </template>
-                        <template v-if="!advanced">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-3" fill="none" viewBox="0 0 24 24"
-                                 stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                      d="M5 11l7-7 7 7M5 19l7-7 7 7"/>
-                            </svg>
-                        </template>
+                        <SelectButton v-model="form.operator" :options="operatorOptions"
+                                      optionLabel="name" optionValue="value"/>
                     </div>
                 </div>
 
-                <jet-button :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Search
-                </jet-button>
-
-                <div :class="['col-span-6', {'h-0' : !advanced}]">
-                    <div :class="['grid grid-cols-8 gap-2 border border-gray-100 transition transition-all duration-100 ease-in-out transform',
-                            {'opacity-0' : !advanced},
-                            {'translate-y-12' : !advanced},
-                            {'opacity-100' : advanced},
-                    ]">
-                        <div class="col-span-2">
-                            <div class="mt-3">
-                                <InputSwitch v-model="checked"
-                                             onchange="onChangePhraseMode"
-                                             name="Exact Phrase"
-                                             :initial-state="form.isPhrase  ? '1' : '0'"
-                                             left-text="No"
-                                             right-text="Yes"/>
-                            </div>
-                        </div>
-                        <div class="col-span-2">
-                            <div class="mt-3">
-                                <InputSwitch v-model="checked"
-                                             onchange="onChangeOCR"
-                                             name="OCR"
-                                             :initial-state="form.isOCR ? '1' : '0'"
-                                             left-text="No"
-                                             right-text="Yes"/>
-                            </div>
-                        </div>
-                        <div class="col-span-2">
-                            <div class="mt-3">
-                                <InputSwitch v-model="checked"
-                                             onchange="onChangeType"
-                                             name="Type"
-                                             :initial-state="form.type==='thumbnail' ? '1' : '0'"
-                                             left-text="MySGS"
-                                             right-text="Thumbnail"/>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <Button type="submit" @click="doSearch"
+                        label="Search" severity="success"/>
 
 
             </template>
