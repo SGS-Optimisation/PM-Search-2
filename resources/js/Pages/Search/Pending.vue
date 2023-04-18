e2474d0e9f9ed58945b621394653905f0d897f0e
khushboo.chhabra <khushbooChhabra.ManmohanSingh@sgsco.com>
2023-04-18 16:53:39 +0530

    fixing spinner.

--- resources/js/Pages/Search/Pending.vue
+++ resources/js/Pages/Search/Pending.vue
@@ -2,6 +2,7 @@
 import ProgressSpinner from 'primevue/progressspinner';
 import axios from "axios";
 import route from "ziggy-js";
+import {Head, useForm, usePage} from "@inertiajs/vue3";
 import AppLayout from "@/Layouts/AppLayout.vue";
 import { onMounted } from "vue";
 
@@ -32,33 +33,32 @@ onMounted(() => {
 </script>
 
 <template>
-    <app-layout>
-        <template #header>
-                    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                        <template v-if="search.search_mode === 'image'">
-                            Awaiting result for file {{ filename }}
-                        </template>
-                        <template v-else>
-                            <p>Searching for
-                                <em>{{search.search_data.search_string.join(' ' + search.search_data.operator.toUpperCase() + ' ')}}</em>
-                            </p>
-                        </template>
-                    </h2>
+    <div class="py-12">
+        <header class="flex flex-col items-center justify-center">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    <template v-if="search.search_mode === 'image'">
+                        Awaiting result for file {{ filename }}
+                    </template>
+                    <template v-else>
+                        <p>Searching for
+                            <em>{{search.search_data.search_string.join(' ' + search.search_data.operator.toUpperCase() + ' ')}}</em>
+                        </p>
+                    </template>
+                </h2>
+            </div>
+        </header>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="flex flex-col items-center justify-center p-2 overflow-hidden sm:rounded-md">
+                <template v-if="error || search.working_data.hasOwnProperty('error') && search.working_data.error">
+                    An error has occured. Please try again later.
                 </template>
-
-                <div class="py-12">
-                    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                        <div class="bg-white p-2 overflow-hidden shadow-xl sm:rounded-lg">
-                            <template v-if="error || search.working_data.hasOwnProperty('error') && search.working_data.error">
-                                An error has occured. Please try again later.
-                            </template>
-                            <template v-else>
-                                <ProgressSpinner/>
-                            </template>
-                        </div>
-                    </div>
-                </div>
-    </app-layout>
+                <template v-else>
+                    <ProgressSpinner style="width: 50px; height: 50px;" />
+                </template>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
