8a6e548bf32e9fbfd99860d9d050c7858f27a0d3
Yann Labour <yann.labour@sgsco.com>
2023-06-08 14:43:14 +0400

    adds horizon queue worker

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -6,6 +6,7 @@
     composer install
     sudo service php8.2-fpm restart
     php8.2 artisan config:clear
+    php8.2 artisan cache:clear
     php8.2 artisan migrate --force
     sudo supervisorctl restart pm-search-queue-worker:
     /home/mytransferdev/.nvm/versions/node/v16.16.0/bin/npm install
